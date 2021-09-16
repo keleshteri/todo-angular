2163fc0735948527b03d31411cfd0a98de2eea84
m.mehdi shaban keleshteri <mehdi.shaban.keleshteri@gmail.com>
2021-09-16 21:46:36 +0800

    Toggle Reminder

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -11,6 +11,7 @@ export class TaskItemComponent implements OnInit {
 
   @Input() task!:Task
   @Output() onDeleteTask:EventEmitter<Task>= new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task>= new EventEmitter();
   faTime = faTimes;
 
   constructor() { }
@@ -18,8 +19,11 @@ export class TaskItemComponent implements OnInit {
   ngOnInit(): void {}
 
   onDelete(task:Task){
-    
-    this.onDeleteTask.emit(task);
-    
+    this.onDeleteTask.emit(task);  
   }
+
+  onToggle(task:Task){
+    this.onToggleReminder.emit(task);
+  }
+
 }
